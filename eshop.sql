0558122f8b73b632e40e75f3a91d41fafbdc44f2
jhonn-jordan <adembigondjo@gmail.com>
2022-08-30 09:36:51 +0100

    'sql'

--- eshop.sql
+++ eshop.sql
@@ -2,23 +2,9 @@ CREATE DATABASE eshop;
 
 USE eshop
 
-CREATE TABLE `chaussures` (
-  `idxChaussure` int(9) NOT NULL AUTO_INCREMENT,
-  `idxMarque` int(9) NOT NULL,
-  `taille` int(2) NOT NULL,
-  `couleur` varchar(20) NOT NULL,
-  `prix` float NOT NULL,
-  `nomChaussure` varchar(25) NOT NULL,
-  PRIMARY KEY (`idxChaussure`),
-  KEY `idxMarque` (`idxMarque`)
-) ENGINE=Innodb DEFAULT CHARSET=utf8;
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 
-
-INSERT INTO `chaussures` (`idxChaussure`, `idxMarque`, `taille`, `couleur`, `prix`, `nomChaussure`) VALUES
-(1, 3, 45, 'rouge', 50000, 'SPARCO'),
-(2, 1, 35, 'gris', 23000, 'ADENCO'),
-(3, 2, 40, 'Jaune', 40000, 'NIKE ZOOM');
-
+SELECT User, Host FROM mysql.user;
 
 CREATE TABLE `clients` (
   `idxClient` int(9) NOT NULL AUTO_INCREMENT,
@@ -35,16 +21,10 @@ CREATE TABLE `commandes` (
   `idxClient` int(9) NOT NULL,
   `date` varchar(10) NOT NULL,
   PRIMARY KEY (`idxCommande`),
-  KEY `client_idx` (`idxClient`)
-) ENGINE=Innodb DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `detailcommandes` (
-  `idxCommande` int(9) NOT NULL AUTO_INCREMENT,
-  `idxChaussure` int(9) NOT NULL,
-  `quantite` int(3) NOT NULL,
-  PRIMARY KEY (`idxCommande`),
-  KEY `idxChaussure` (`idxChaussure`)
+  INDEX par_ind (`idxClient`),
+    FOREIGN KEY (`idxClient`)
+        REFERENCES clients(`idxClient`)
+        ON DELETE CASCADE
 ) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
 
@@ -56,10 +36,44 @@ CREATE TABLE `marques` (
 ) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
 
+CREATE TABLE `chaussures` (
+  `idxChaussure` int(9) NOT NULL AUTO_INCREMENT,
+  `idxMarque` int(9) NOT NULL,
+  `taille` int(2) NOT NULL,
+  `couleur` varchar(20) NOT NULL,
+  `prix` float NOT NULL,
+  `nomChaussure` varchar(25) NOT NULL,
+  PRIMARY KEY (`idxChaussure`),  
+  INDEX par_ind (`idxMarque`),
+    FOREIGN KEY (`idxMarque`)
+        REFERENCES marques(`idxMarque`)
+        ON DELETE CASCADE
+) ENGINE=Innodb DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `detailcommandes` (
+  `idxCommande` int(9) NOT NULL AUTO_INCREMENT,
+  `idxChaussure` int(9) NOT NULL,
+  `quantite` int(3) NOT NULL,
+  PRIMARY KEY (`idxCommande`),
+  INDEX par_ind (`idxChaussure`),
+    FOREIGN KEY (`idxChaussure`)
+        REFERENCES chaussures(`idxChaussure`)
+        ON DELETE CASCADE
+  
+) ENGINE=Innodb DEFAULT CHARSET=utf8;
+
+
 INSERT INTO `marques` (`idxMarque`, `marque`, `logo`) VALUES
 (1, 'Adidas', 'LogoAdidas'),
 (2, 'Nike', 'LogoNike'),
 (3, 'Puma', 'LogoPuma');
+
+
+INSERT INTO `chaussures` (`idxChaussure`, `idxMarque`, `taille`, `couleur`, `prix`, `nomChaussure`) VALUES
+(1, 3, 45, 'rouge', 50000, 'SPARCO'),
+(2, 1, 35, 'gris', 23000, 'ADENCO'),
+(3, 2, 40, 'Jaune', 40000, 'NIKE ZOOM');
 
 
 -- LISTE LES MARQUES CONTENUS DANS DANS LA TABLES CHAUSSURES
